8961ef6c1f7d9a635bd8309ecb740c1340b49bce
Twilghts <r2092712744@Gmail.com>
2023-08-29 19:15:46 +0800

    得到新模型，性能不佳。 修改如下： 1.当本batch中某路由器没有传感类或通信类任务时返回的奖励值从0.03改为0.15。 2.传感类任务的数据包数量从(2, 12)变为(4, 15)。通信类数据包的数量从(2, 8)改为(2, 11)。 3.计算类任务的奖励函数中正态分布的标准差从0.7变为1.0

--- service/servicefactory.py
+++ service/servicefactory.py
@@ -21,10 +21,10 @@ class TaskFactory:
                     path: Union[List[int], None] = None) -> Task:
         if task_type == TypeOfTask.communication_task:
             return CommunicationTask(slice_sign=slice_sign, path=path, task_id=task_id, data_id=data_id,
-                                     communication_required=random.randint(2, 8))
+                                     communication_required=random.randint(2, 11))
         elif task_type == TypeOfTask.calculate_task:
             return CalculateTask(slice_sign=slice_sign, task_id=task_id, data_id=data_id,
                                  calculate_required=random.randint(1, 3))
         elif task_type == TypeOfTask.sensor_task:
             return SensorTask(slice_sign=slice_sign, path=path, task_id=task_id, data_id=data_id,
-                              sensor_required=random.randint(2, 12), specific_type=random.randint(1, 2))
+                              sensor_required=random.randint(4, 15), specific_type=random.randint(1, 2))
