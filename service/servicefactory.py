e658c4f0d7b58ec15c91ce3fdf0ca555e774e164
Twilghts <r2092712744@Gmail.com>
2023-08-30 22:55:06 +0800

    本次模型效果不佳，修改的参数如下： 1.传感类任务的数据包数量从(3, 15)变为(2, 12)，通信类任务的数据包数量从(2, 11)变为(2, 10)。 2.计算和通信(传感的通信部分)类任务的奖励函数中正态分布的标准差从0.9变为0.95。 3.计算类任务的初始资源分配从(0.2, 0.6, 0.2)修改为(0.1, 0.8, 0.1)

--- service/servicefactory.py
+++ service/servicefactory.py
@@ -21,10 +21,10 @@ class TaskFactory:
                     path: Union[List[int], None] = None) -> Task:
         if task_type == TypeOfTask.communication_task:
             return CommunicationTask(slice_sign=slice_sign, path=path, task_id=task_id, data_id=data_id,
-                                     communication_required=random.randint(2, 11))
+                                     communication_required=random.randint(2, 10))
         elif task_type == TypeOfTask.calculate_task:
             return CalculateTask(slice_sign=slice_sign, task_id=task_id, data_id=data_id,
                                  calculate_required=random.randint(1, 3))
         elif task_type == TypeOfTask.sensor_task:
             return SensorTask(slice_sign=slice_sign, path=path, task_id=task_id, data_id=data_id,
-                              sensor_required=random.randint(3, 15), specific_type=random.randint(1, 2))
+                              sensor_required=random.randint(2, 12), specific_type=random.randint(1, 2))
