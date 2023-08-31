36bbf9ce26b43f5958e6842c096d93d025859e55
Twilghts <r2092712744@Gmail.com>
2023-08-31 18:19:54 +0800

    本次模型效果不佳，修改的参数如下： 1.计算类任务的初始资源分配从(0.1, 0.8, 0.1)修改为(0.15, 0.7, 0.15)。

--- net_related/net.py
+++ net_related/net.py
@@ -156,9 +156,9 @@ class Net:
             router.distribution[1][2] = 0.15
             router.distribution[1][3] = 0.35
             # 针对路由器计算资源的初始化，切片二是主要处理计算业务的
-            router.distribution[2][1] = 0.2
-            router.distribution[2][2] = 0.6
-            router.distribution[2][3] = 0.2
+            router.distribution[2][1] = 0.15
+            router.distribution[2][2] = 0.7
+            router.distribution[2][3] = 0.15
             # 针对路由器存储资源的初始化，切片三是主要处理存储业务的
             router.distribution[3][1] = 0.15
             router.distribution[3][2] = 0.15
