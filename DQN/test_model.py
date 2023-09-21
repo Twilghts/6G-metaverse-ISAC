a82be654ea39a4f738355a16903a7305a7524dc9
Twilghts <r2092712744@Gmail.com>
2023-09-21 11:18:50 +0800

    模型83：本次模型效果有史最好(传感和通信方面优于对照组，计算劣于对照组，通信和传感有进步) 具体数据如下： DQN传感丢包率：0.00006150818058801821 对照组丢包率：0 DQN传感：2.437220,0.006137,0.263420 对照组传感：3.470880,0.006538,0.524717 DQN通信：7.078571,0.006122,0.448415 对照组通信：2.010084,0.006857,0.549234 DQN计算：0.732984,0.002772,0.037379 对照组计算：0.218676,0.003408,0.023056 修改的参数如下： 1.计算数据包的数量从(2, 6)调整到(4, 12)

--- DQN/test_model.py
+++ DQN/test_model.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     start_time = time.perf_counter()
     test_net = Net()
     for router in test_net.core_routers.values():
-        filename = "model_83_" + str(router.sign)
+        filename = "model_84_" + str(router.sign)
         router.agent.target_model = tf.keras.models.load_model(f"../resource/{filename}")
     test_net.initialize()
     paths = test_net.chose_paths()
